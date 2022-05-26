417506e65309d6c1c58240521be3af3965c563e9
Sadullayev06 <sadullayevzufarbek4@gmail.com>
2022-05-26 15:07:36 +0500

    console update

--- src/main/java/uz/isystem/ShopUz/model/Address.java
+++ src/main/java/uz/isystem/ShopUz/model/Address.java
@@ -1,4 +1,17 @@
 package uz.isystem.ShopUz.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+
+@Entity
+@Table(name = ("address"))
 public class Address {
+       @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
 }
